541ead04ce31d08b837b8fbafecf19b9a44d7963
sonic5739 <83488974+sonic5739@users.noreply.github.com>
2023-10-21 15:26:37 +0900

    Delete time.js

--- time.js
+++
@@ -1,9 +0,0 @@
-const date  = new Date() ;
-// const year = date.getFullYear();
-// const month  = date.getMonth() +1 ;
-// const day  = date.getDate();
-const locale  = date.toLocaleString();
-const test = setInterval(() => {
-    const text  = `현재시간은 ${locale} 입니다.`
-    document.querySelector('.time').innerHTML = text
-}, 1000)
